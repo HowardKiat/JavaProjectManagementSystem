32112522aef44e21713ccce1265881d16de9b6de
HowardKiat <buisnesskiat123@gmail.com>
2024-05-07 17:36:59 +0800

    Bug FIxes: Images are now importable (Do not touch/Push this is working version.)

--- src/main/java/com/mycompany/javaprojectmanagementsystem/splash.java
+++ src/main/java/com/mycompany/javaprojectmanagementsystem/splash.java
@@ -4,11 +4,13 @@
  */
 package com.mycompany.javaprojectmanagementsystem;
 
+import javax.swing.JFrame;
+
 /**
  *
  * @author Darren
  */
-public class splash extends javax.swing.JFrame {
+public class splash extends JFrame {
 
     /**
      * Creates new form splash
@@ -28,8 +30,8 @@ public class splash extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
         Percentage = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
         ProgressBar = new javax.swing.JProgressBar();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -42,13 +44,13 @@ public class splash extends javax.swing.JFrame {
         jLabel1.setForeground(new java.awt.Color(255, 255, 255));
         jLabel1.setText("School Management System");
 
-        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icon/school (1).png"))); // NOI18N
-
         Percentage.setBackground(new java.awt.Color(255, 255, 255));
         Percentage.setFont(new java.awt.Font("Tahoma", 1, 36)); // NOI18N
         Percentage.setForeground(new java.awt.Color(255, 255, 255));
         Percentage.setText("%");
         Percentage.setToolTipText("");
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/school (1).png"))); // NOI18N
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -59,9 +61,9 @@ public class splash extends javax.swing.JFrame {
                 .addComponent(jLabel1)
                 .addGap(59, 59, 59))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addGap(105, 105, 105)
-                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 142, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(89, 89, 89)
+                .addGap(86, 86, 86)
+                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 148, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(275, 275, 275)
                 .addComponent(Percentage, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
         );
@@ -72,12 +74,13 @@ public class splash extends javax.swing.JFrame {
                 .addComponent(jLabel1)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(25, 25, 25)
-                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 228, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(80, 80, 80)
-                        .addComponent(Percentage, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(27, Short.MAX_VALUE))
+                        .addComponent(Percentage, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(74, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 163, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(54, 54, 54))))
         );
 
         ProgressBar.setBackground(new java.awt.Color(255, 255, 255));
@@ -95,7 +98,7 @@ public class splash extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(ProgressBar, javax.swing.GroupLayout.DEFAULT_SIZE, 9, Short.MAX_VALUE))
+                .addComponent(ProgressBar, javax.swing.GroupLayout.DEFAULT_SIZE, 5, Short.MAX_VALUE))
         );
 
         pack();
@@ -113,7 +116,7 @@ public class splash extends javax.swing.JFrame {
             {
                 Thread.sleep(20);
                 Mysplash.ProgressBar.setValue(i);
-                Mysplash.Percentage.setText(Integer.valueOf(i)+ "%");
+                Mysplash.Percentage.setText(i+ "%");
             }
 
          
