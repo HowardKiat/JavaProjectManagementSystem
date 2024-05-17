ae7ec2afd6e9c672436e5ee7a1fd4c3a798315a8
HowardKiat <buisnesskiat123@gmail.com>
2024-05-17 23:31:22 +0800

    fixed date format, integrated more OOP functions. Fixed commit issues. Better parsing and error handling. Will work on new features tmr

--- src/main/java/com/mycompany/javaprojectmanagementsystem/lecturer.java
+++ src/main/java/com/mycompany/javaprojectmanagementsystem/lecturer.java
@@ -248,6 +248,9 @@ public class lecturer extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(lecturer.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
