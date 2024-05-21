e910dad2ade0c38d3e92cc95ca1654371729f655
HowardKiat <buisnesskiat123@gmail.com>
2024-05-21 10:53:21 +0800

    conflicting class names student and Student (Think of using caps in front file)

--- src/main/java/com/mycompany/javaprojectmanagementsystem/login.java
+++ src/main/java/com/mycompany/javaprojectmanagementsystem/login.java
@@ -5,7 +5,7 @@
 package com.mycompany.javaprojectmanagementsystem;
 
 import com.mycompany.javaprojectmanagementsystem.admin;
-import com.mycompany.javaprojectmanagementsystem.student;
+import com.mycompany.javaprojectmanagementsystem.Student;
 import java.awt.Color;
 import java.awt.Font;
 import java.io.BufferedReader;
@@ -422,7 +422,7 @@ public class login extends javax.swing.JFrame {
                 new lecturer(name).setVisible(true);
                 break;
             case "student":
-                new student(name).setVisible(true);
+                new Student(name).setVisible(true);
                 break;
             default:
                 JOptionPane.showMessageDialog(null, "Invalid role selected", "Error", JOptionPane.ERROR_MESSAGE);
